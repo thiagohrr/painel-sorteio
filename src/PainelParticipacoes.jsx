9edb515e04da772a23259fd411393cb29f0e2df5
Thiago Henrique Rabelo Ribeiro <thiagohrr@gmail.com>
2025-04-01 12:21:58 -0300

    Primeiro commit do projeto completo

---
+++ src/PainelParticipacoes.jsx
@@ -0,0 +1,2 @@
+// Substitua este conteúdo pelo painel salvo anteriormente.
+console.error("O código do PainelParticipacoes foi perdido durante a reinicialização do ambiente.");
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PainelParticipacoes from './PainelParticipacoes'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PainelParticipacoes />
+  </React.StrictMode>,
+)
